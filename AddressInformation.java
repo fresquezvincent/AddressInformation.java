53ab1014ec86ae8d5e4ac1b84690eb4b331300f4
fresquezvincent <fresquezvincent@gmail.com>
2026-01-19 20:00:11 -0700

    Add files via upload

---
+++ AddressInformation.java
@@ -0,0 +1,12 @@
+public class AddressInformation {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Name: Marcus");
+        System.out.println("Last Name: Delgado");
+        System.out.println("Street Address: 742 Copper Ridge Lane");
+        System.out.println("City: Denver");
+        System.out.println("Zip Code: 80216");
+
+    }
+}
